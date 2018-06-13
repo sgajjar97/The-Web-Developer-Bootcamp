3884072047074e543758497b2103ec438591284a
Shail Gajjar <sgajjar97@gmail.com>
2018-06-13 14:37:12 -0700

    Add files via upload

---
+++ JAVASCRIPT UNIT/annoy.js
@@ -0,0 +1,14 @@
+var answer = prompt("Are we there yet?");
+
+while (answer.indexOf("yes") === -1) {
+	var answer = prompt("Are we there yet?")
+}
+
+alert("YAY, we made it!")
+
+// if (answer === "yes"){
+// 	alert("Yay, we made it")
+// }
+// else {
+
+// }
