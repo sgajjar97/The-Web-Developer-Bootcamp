beac2962398a495042d89d2a28aa4859f4e61f5f
Shail Gajjar <sgajjar97@gmail.com>
2018-06-20 14:56:57 -0700

    Add files via upload

--- JAVASCRIPT UNIT/todolist.js
+++ JAVASCRIPT UNIT/todolist.js
@@ -7,13 +7,35 @@ var input = prompt("What would you like to do?");
 while (input !== "quit") {
 
 	if (input === "list") {
-	console.log(todos);
+		listTodos();
 	} 
 	else if (input === "new") {
-	var newTodo = prompt("Enter new todo");
-	todos.push(newTodo);
+		addTodo();
+	}
+	else if (input === "delete") {
+		deleteTodo()
 	}
 
 	input = prompt("What would you like to do?");
 }
 console.log("OK, you quit the app!");
+
+function listTodos() {
+	console.log("**********");
+		todos.forEach(function(todo, index){
+			console.log(index + ": " + todo);
+		});
+	console.log("**********");
+}
+
+function addTodo() {
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+function deleteTodo() {
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
