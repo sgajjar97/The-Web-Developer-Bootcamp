092744fadd4af6b9c2a13012ece3a07e1a8ee1ce
Shail Gajjar <sgajjar97@gmail.com>
2018-09-05 12:46:21 -0700

    Add files via upload

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -2,12 +2,19 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("search"); 
+});
 
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=car&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, function(error, response, body){
        if(!error && response.statusCode == 200) {
-           var results = JSON.parse(body);
-           res.send(results["Search"][0]["Title"]);
+           var data = JSON.parse(body);
+           res.render("results", {data: data});
         } 
     });
 });
