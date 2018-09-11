5bf1ba673f8e0ee3787d2a6a90d0ddc8c246aef2
Shail Gajjar <sgajjar97@gmail.com>
2018-09-10 20:33:53 -0700

    Add files via upload

---
+++ YelpCamp/v1/app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+            {name: "Salmon Creek", image: "https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg"},
+            {name: "Granite Hill", image: "https://s3-us-west-2.amazonaws.com/hispotion-prod/wp-content/uploads/2017/05/31-05101657f53d1a399b7051016886742565-31.jpg"},
+            {name: "Red Rock", image: "https://cdn.mpora.com/featured_image/5ab90f8109f79.jpg"}
+        ]
+
+app.get("/", function(req, res){
+    res.render("landing"); 
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+   //get data from form and add to campgrounds array
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   campgrounds.push(newCampground);
+   
+   //redirect back to campgrounds page
+   res.redirect("/campgrounds");
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new.ejs"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("YelpCamp server started!");
+});
