34a3c835ab9d02b55c99db5fc20c2d47b98583e0
Shail Gajjar <sgajjar97@gmail.com>
2018-09-17 18:33:58 -0700

    Add files via upload

---
+++ YelpCamp/v2/app.js
@@ -0,0 +1,81 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=350"
+//     }, function(err, campground) {
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("NEWLY CREATED CAMPGROUnD: ");
+//             console.log(campground);
+//         }
+        
+//     });
+
+var campgrounds = [
+            {name: "Salmon Creek", image: "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Granite Hill", image: "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Salmon Creek", image: "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Granite Hill", image: "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Salmon Creek", image: "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Granite Hill", image: "https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?auto=compress&cs=tinysrgb&h=350"},
+            {name: "Red Rock", image: "https://s.hswstatic.com/gif/landscape-photography-1.jpg"}
+        ]
+
+app.get("/", function(req, res){
+    res.render("landing"); 
+});
+
+app.get("/campgrounds", function(req, res){
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+});
+
+app.post("/campgrounds", function(req, res){
+   //get data from form and add to campgrounds array
+   var name = req.body.name;
+   var image = req.body.image;
+   var newCampground = {name: name, image: image};
+   //Creat a new campground and save to DB
+   Campground.create(newCampground, function(err, newlyCreated){
+       if (err) {
+           console.log(err);
+       } else {
+        //redirect back to campgrounds page
+           res.redirect("/campgrounds");
+       }
+   });
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new.ejs"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("YelpCamp server started!");
+});
