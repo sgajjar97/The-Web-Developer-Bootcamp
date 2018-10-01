6ff27cc2d7823c5d713d41743a61c9e066d9191d
Shail Gajjar <sgajjar97@gmail.com>
2018-10-01 14:00:06 -0700

    Add files via upload

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -1,8 +1,9 @@
-var bodyParser = require("body-parser"),
-methodOverride = require("method-override"),
-mongoose       = require("mongoose"),
-express        = require("express"),
-app            = express();
+var bodyParser   = require("body-parser"),
+methodOverride   = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
+mongoose         = require("mongoose"),
+express          = require("express"),
+app              = express();
 
 //APP Config
 mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true});
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //MONGOOSE/Model Config
 var blogSchema = new mongoose.Schema({
@@ -52,6 +54,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE route
 app.post("/blogs", function(req, res) {
   //create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog){
      if (err){
          res.render("new");
@@ -86,6 +89,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //UPDATE route
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if (err){
             res.redirect("/blogs");
