8433b96df989b2d1471b9ee7db660d20082e2a94
Shail Gajjar <sgajjar97@gmail.com>
2018-09-30 22:45:33 -0700

    Add files via upload

--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -95,6 +95,18 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+//DELETE route
+app.delete("/blogs/:id", function(req, res) {
+   //destroy blog
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started!");
