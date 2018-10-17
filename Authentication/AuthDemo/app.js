386db6e2a0f22fd08b2c9543b5e3d2863088e206
Shail Gajjar <sgajjar97@gmail.com>
2018-10-16 20:53:15 -0700

    Add files via upload

---
+++ Authentication/AuthDemo/app.js
@@ -0,0 +1,43 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Its a secret world!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//============
+// ROUTES
+//============
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret", function(req, res){
+   res.render("secret"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started...");
+})
