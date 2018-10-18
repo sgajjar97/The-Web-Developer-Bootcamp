70d1cc37b01b6142a2d02b17759b18bd1d56fcf6
Shail Gajjar <sgajjar97@gmail.com>
2018-10-18 15:13:04 -0700

    Add files via upload

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -22,6 +22,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -34,7 +35,7 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret",isLoggedIn, function(req, res){
    res.render("secret"); 
 });
 
@@ -57,6 +58,33 @@ app.post("/register", function(req, res){
         });
     });
 });
+
+//LOGIN ROUTES
+
+//render login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started...");
